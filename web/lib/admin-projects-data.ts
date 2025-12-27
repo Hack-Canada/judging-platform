b98a468d4197703a5d54dcc44131fb8b53e65658
faizm10 <faizmustansar10@gmail.com>
2025-12-27 02:06:29 -0500

    removed old stuff for new migration

--- web/lib/admin-projects-data.ts
+++
@@ -1,9 +0,0 @@
-export type AdminProject = {
-  id: number
-  name: string
-  assignedJudges: string[]
-  totalInvestment: number
-  tracks: string[] // Track names (e.g. ["General", "RBC Track"])
-  submissionId?: string // UUID from submissions table
-}
-
